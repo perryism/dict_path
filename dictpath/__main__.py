cf1ce3b27496fde52e98b81d5c77f5c301d7388f
Perry <perryism@gmail.com>
2019-10-15 13:48:06 -0700

    Don't use pprint

--- dictpath/__main__.py
+++ dictpath/__main__.py
@@ -1,9 +1,7 @@
 from dictpath import dictpath
 import sys, json
-import pprint
 
 if __name__ == '__main__':
     d = json.loads(sys.stdin.read())
-    pp = pprint.PrettyPrinter(indent=1)
-    pp.pprint(dictpath(d).explore())
+    print(json.dumps(dictpath(d).explore()))
 
